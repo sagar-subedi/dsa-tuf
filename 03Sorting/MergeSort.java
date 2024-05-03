fa4df6ce52daee6f438e2dd5f3dbbc280aabec30
Sagar Subedi <sagarxic@gmail.com>
2024-05-03 13:07:07 +0545

    merge sort

--- 03Sorting/MergeSort.java
+++ 03Sorting/MergeSort.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
-        System.out.println("Merge Sort");
+        int[] arr = {3,2,1,5,4,2,8,7,6};
+        mergeSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid+1;
+
+
+        while (left <= mid && right <=high) {
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else{
+                temp.add(arr[right]);
+                right++;
+            }
+
+
+        }
+
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i = low; i <= high; i++){
+            arr[i] = temp.get(i-low);
+        }
+
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if(low>=high) return;
+        int mid = (low+high)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+        merge(arr, low, mid, high);
     }
 }
