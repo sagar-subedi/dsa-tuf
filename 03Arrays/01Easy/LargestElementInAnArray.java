7d5ffa55721da49598a1b9326527be63111b4287
Sagar Subedi <sagarxic@gmail.com>
2024-05-08 23:31:25 +0545

    Implemented largest element in an arry

---
+++ 03Arrays/01Easy/LargestElementInAnArray.java
@@ -0,0 +1,17 @@
+import Utils.ArrayUtils;
+
+public class LargestElementInAnArray {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtils.getArrayFromInput();
+        System.out.println(findLargestElement(arr));
+        
+    }
+
+    public static int findLargestElement(int[] arr){
+        int largest = 0;
+        for(int e: arr){
+            if(e>largest) largest = e;
+        }
+        return largest;
+    }
+}
--- Utils/ArrayUtils.java
+++ Utils/ArrayUtils.java
@@ -1,9 +1,25 @@
 package Utils;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayUtils {
     public static void printArray(int[] arr){
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] getArrayFromInput(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+        String[] arr = input.split(" ");
+        int[] arrInt = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            arrInt[i] = Integer.parseInt(arr[i]);
+        }
+        return arrInt;
+
+
     }
 }
