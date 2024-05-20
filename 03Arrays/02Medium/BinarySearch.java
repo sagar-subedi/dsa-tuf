0478ab2cf3c6eec0ca8a736420a10307a945af11
Sagar Subedi <sagarxic@gmail.com>
2024-05-20 22:05:56 +0545

    Implement: Binary Search Recursive

--- 03Arrays/02Medium/BinarySearch.java
+++ 03Arrays/02Medium/BinarySearch.java
@@ -1,11 +1,11 @@
 public class BinarySearch{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
-        System.out.println(binarySearch(arr,4));
+        System.out.println(binarySearchRecursive(arr,0, arr.length-1,4));
         
     }
 
-    public static int binarySearch(int[] arr, int num){
+    public static int binarySearchIterative(int[] arr, int num){
         int left = 0;
         int right = arr.length-1;
         int mid = 0;
@@ -21,6 +21,21 @@ public class BinarySearch{
             else if(arr[mid]>num){
                 right = mid;
             }
+        }
+        return -1;
+    }
+
+    public static int binarySearchRecursive(int[] arr, int left, int right, int num){
+        int mid = left+(right-left)/2;
+
+        if(arr[mid] == num){
+            return mid;
+        }
+        else if(arr[mid] < num){
+            return binarySearchRecursive(arr, mid+1, right, num);
+        }
+        else if(arr[mid] > num) {
+            return binarySearchRecursive(arr, left, mid-1, num);
         }
         return -1;
     }
